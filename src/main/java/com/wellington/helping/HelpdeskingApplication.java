2ba520f1f24ff9b4d71360a6c6e6b46f877a5d8c
Wellington Feijó <wellingtonfc95@gmail.com>
2023-01-02 17:24:25 -0300

    projeto iniciado

---
+++ src/main/java/com/wellington/helping/HelpdeskingApplication.java
@@ -0,0 +1,13 @@
+package com.wellington.helping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellington/helping/HelpdeskingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellington.helping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
