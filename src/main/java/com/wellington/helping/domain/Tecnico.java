8c94b326f972adcda23ae11d29179534fef518cb
Wellington Feijó <wellingtonfc95@gmail.com>
2023-01-03 14:18:32 -0300

    Anotacoes JPA para classes de dominio

--- src/main/java/com/wellington/helping/domain/Tecnico.java
+++ src/main/java/com/wellington/helping/domain/Tecnico.java
@@ -3,16 +3,28 @@ package com.wellington.helping.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.wellington.helping.domain.enums.Perfil;
+
+@Entity
+
 public class Tecnico extends Pessoa{
 
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList();
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
