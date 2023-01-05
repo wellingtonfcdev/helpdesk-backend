285c65a943f52f208f7f24f95e23ec0eca6865c0
Wellington Feijó <wellingtonfc95@gmail.com>
2023-01-05 01:54:04 -0300

    Perfil desenvolvimento

---
+++ src/main/java/com/wellington/helping/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.wellington.helping.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.wellington.helping.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	@Bean
+	public boolean instanciaDb() {
+		if(value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		return false;
+	}
+
+}
