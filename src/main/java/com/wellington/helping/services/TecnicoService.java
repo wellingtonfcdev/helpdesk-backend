12effc0b56df5c55e420c5c822e93b177f39754a
Wellington Feijó <wellingtonfc95@gmail.com>
2023-01-10 01:39:46 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/wellington/helping/services/TecnicoService.java
+++ src/main/java/com/wellington/helping/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.wellington.helping.domain.Tecnico;
 import com.wellington.helping.repositories.TecnicoRepository;
+import com.wellington.helping.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional <Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! id:"+ id));
 	}
 }
