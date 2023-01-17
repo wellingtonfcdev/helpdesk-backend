55d5b3ae79588ba9e1a348f343a0773d437322c6
Wellington Feijó <wellingtonfc95@gmail.com>
2023-01-17 12:04:08 -0300

    Endpoint create para tecnico

--- src/main/java/com/wellington/helping/domain/Tecnico.java
+++ src/main/java/com/wellington/helping/domain/Tecnico.java
@@ -2,11 +2,13 @@ package com.wellington.helping.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.wellington.helping.domain.dtos.TecnicoDTO;
 import com.wellington.helping.domain.enums.Perfil;
 
 @Entity
@@ -29,6 +31,18 @@ public class Tecnico extends Pessoa{
 		addPerfil(Perfil.CLIENTE);
 	}
 
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+		addPerfil(Perfil.CLIENTE);
+	}
+		
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
--- src/main/java/com/wellington/helping/services/TecnicoService.java
+++ src/main/java/com/wellington/helping/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wellington.helping.domain.Tecnico;
+import com.wellington.helping.domain.dtos.TecnicoDTO;
 import com.wellington.helping.repositories.TecnicoRepository;
 import com.wellington.helping.services.exceptions.ObjectnotFoundException;
 
@@ -23,5 +24,11 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
