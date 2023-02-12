0c673cf59d736533fb66bf58dc1b06b4f9deae5a
Wellington Feijó <wellingtonfc95@gmail.com>
2023-02-12 02:27:09 -0300

    Implementação metodo create

--- src/main/java/com/wellington/helping/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/wellington/helping/domain/dtos/ChamadoDTO.java
@@ -3,6 +3,8 @@ package com.wellington.helping.domain.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.wellington.helping.domain.Chamado;
 
@@ -15,11 +17,17 @@ public class ChamadoDTO implements Serializable{
 	private LocalDate dataAbertura = LocalDate.now();
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	@NotNull(message = "O campo PRIORIDADE é requerido")
 	private Integer prioridade;
+	@NotNull(message = "O campo STATUS é requerido")
 	private Integer status;
+	@NotNull(message = "O campo TITULO é requerido")
 	private String titulo;
+	@NotNull(message = "O campo OBSERVAÇÕES é requerido")
 	private String observacoes;
+	@NotNull(message = "O campo TECNICO é requerido")
 	private Integer tecnico;
+	@NotNull(message = "O campo CLIENTE é requerido")
 	private Integer cliente;
 	private String nomeTecnico;
 	private String nomeCliente;
--- src/main/java/com/wellington/helping/services/ChamadoService.java
+++ src/main/java/com/wellington/helping/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.wellington.helping.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wellington.helping.domain.Chamado;
+import com.wellington.helping.domain.Cliente;
+import com.wellington.helping.domain.Tecnico;
+import com.wellington.helping.domain.dtos.ChamadoDTO;
+import com.wellington.helping.domain.enums.Prioridade;
+import com.wellington.helping.domain.enums.Status;
 import com.wellington.helping.repositories.ChamadoRepository;
 import com.wellington.helping.services.exceptions.ObjectnotFoundException;
 
@@ -16,6 +23,12 @@ public class ChamadoService {
 	@Autowired
 	private ChamadoRepository repository;
 	
+	@Autowired
+	private TecnicoService tecnicoService;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
@@ -24,4 +37,26 @@ public class ChamadoService {
 	public List<Chamado> findAll() {
 		return repository.findAll();
 		}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
+	}
 }
