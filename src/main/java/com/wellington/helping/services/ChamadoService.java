2884163f12a00e450cf3716b736259fa6f706d82
Wellington Feijó <wellingtonfc95@gmail.com>
2023-02-12 01:59:43 -0300

    findAll implementado para o chamado.

--- src/main/java/com/wellington/helping/services/ChamadoService.java
+++ src/main/java/com/wellington/helping/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.wellington.helping.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,4 +20,8 @@ public class ChamadoService {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
 	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
+		}
 }
